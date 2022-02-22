2f6be85db2f1be52acc8e9a71fbcf7ab712a3db6
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-02-22 14:18:49 -0600

    Built out delete button

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {Button} from 'react-bootstrap'
+import {Button} from 'react-bootstrap';
+import TaskDataService from '../services/taskDataService';
 
 function DeleteButton(props) {
-    let id = props.id
+    let id = props.id;
 
     let delBtnStyle = {
         padding: "0rem 0.25rem",
         marginLeft: "0.25rem"
     }
 
+    const handleClick = (e) => {
+        TaskDataService.deleteTask(id);
+    }
+
     return (
             <Button 
-                style={delBtnStyle} className="btn-danger"
-                ><i className="fas fa-trash-alt"></i>
+                style={delBtnStyle} 
+                className="btn-danger"
+                onClick={handleClick}
+            >
+                <i className="fas fa-trash-alt"></i>
             </Button>
     )
 }
